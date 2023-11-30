b8c2a1ca29ef41bed2910244031e1c771e704bed
JacobUSC <jrr18@email.sc.edu>
2023-11-29 20:22:37 -0500

    updated readme and bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # FedoraConfigs
+I created this to automate the post install setup on Fedora spins cinnamon. This only sets up things the way I like. Feel free to fork and change things to your liking. I am working on a more feature rich post install script that will work on many more distros and have selectable options with a GUI.
+<br>
+<br>
+This will run on other desktop environments and distros but is only tested on [Fedora Spins Cinnamon](https://fedoraproject.org/spins/cinnamon/download)
+<br>
+<br>
+This is provided "as is" and there is no warranty or liability for damages
+
+## Install
+To download enter the command:
+```bash
+curl -L http://github.com/JacobUSC/FedoraConfigs/archive/main.tar.gz | tar zxf -
+```
+To run the script:
+```bash
+sudo bash FedoraConfigs-main/fedora-setup.sh
+```
--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,70 +1,103 @@
 #!/bin/bash
 # Jacobs's Fedora Setup Script
-# For Fedora Workstation 39
+# For Fedora Workstation 39 Cinnamon Spin
+# https://fedoraproject.org/spins/cinnamon/download
 # version 1.0
 
 # varaibles
 $INSTALL_DIR
 
+# repeats chars
+# from https://www.cyberciti.biz/faq/repeat-a-character-in-bash-script-under-linux-unix/
+repeat(){
+	local start=1
+	local end=${1:-80}
+	local str="${2:-=}"
+	local range=$(seq $start $end)
+	for i in $range ; do echo -n "${str}"; done
+}
+
+function end_line() {
+    repeat 80 '='; echo
+    echo ""
+}
 
 # startup
 function startup() {
     INSTALL_DIR=$(pwd)
+    echo $INSTALL_DIR
     sudo dnf install git -y
 }
 
 # sets configs
 function set_configs() {
-    echo "setting configs"
+    echo "@@ setting configs @@"
+    repeat 80 '='; echo
     echo "setting dnf configs"
     echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
     echo 'fastestmirror=true' | sudo tee -a /etc/dnf/dnf.conf
     echo 'defaultyes=true' | sudo tee -a /etc/dnf/dnf.conf
     echo "setting nano configs"
-    touch ~/.nanorc
-    echo 'set guidestripe 80' | tee -a ~/.nanorc
-    echo 'set linenumbers' | tee -a ~/.nanorc
-    echo 'set tabstospaces' | tee -a ~/.nanorc
-    echo 'set tabsize 4' | tee -a ~/.nanorc
-    echo 'set backup' | tee -a ~/.nanorc
-    echo 'set autoindent' | tee -a ~/.nanorc
+    cd ~
+    touch .nanorc
+    echo 'set guidestripe 80' >> ~/.nanorc
+    echo 'set linenumbers' >> ~/.nanorc
+    echo 'set tabstospaces' >> ~/.nanorc
+    echo 'set tabsize 4' >> ~/.nanorc
+    echo 'set backup' >> ~/.nanorc
+    echo 'set autoindent' >> ~/.nanorc
     echo "setting vim configs"
-    touch ~/.vimrc
-    echo 'syntax on' | tee -a ~/.vimrc
-    echo 'set ruler' | tee -a ~/.vimrc
-    echo 'set autoindent' | tee -a ~/.vimrc
-    echo 'set number' | tee -a ~/.vimrc
-    echo 'set ignorecase' | tee -a ~/.vimrc
-    echo 'color elflord' | tee -a ~/.vimrc
+    touch .vimrc
+    echo 'syntax on' >> ~/.vimrc
+    echo 'set ruler' >> ~/.vimrc
+    echo 'set autoindent' >> ~/.vimrc
+    echo 'set number' >> ~/.vimrc
+    echo 'set ignorecase' >> ~/.vimrc
+    echo 'color elflord' >> ~/.vimrc
+    cd $INSTALL_DIR
+    end_line
 }
 
 # updates software
 function update_software() {
-    echo "updating software"
+    echo "@@ updating software @@"
+    repeat 80 '='; echo
     sudo dnf upgrade -y
     sudo flatpak update
+    end_line
 }
 
 # update firmware
 function update_firmware() {
-    echo "updateing firmware"
+    echo "@@ updating firmware @@"
+    repeat 80 '='; echo
     sudo fwupdmgr get-devices
     sudo fwupdmgr refresh --force
     sudo fwupdmgr get-updates
     sudo fwupdmgr update
+    end_line
 }
 
 # adds wallpapers
 function add_wallpapers() {
-    cp -r ./wallpapers ~/pictures/
+    echo "@@ adding wallpapers to Pictures directory @@"
+    repeat 80 '='; echo
+    WALLPAPER_DIR="${INSTALL_DIR}/FedoraConfigs-main/wallpapers"
+    echo "adding wallpapers"
+    cp -r $WALLPAPER_DIR ~/Pictures/
+    end_line
 }
 
 # sets terminal colors
+# currently not working
 # theme and script code from Gogh
 # website: http://gogh-co.github.io/Gogh
 # github: https://github.com/Gogh-Co/Gogh
 function set_colors() {
-    echo "setting terminal colors"
+    echo "@@ setting terminal colors @@"
+    repeat 80 '='; echo
+    sudo dnf install dconf-cli -y
+    sudo dnf install uuid-runtime -y
     mkdir -p "$HOME/src"
     cd "$HOME/src"
     git clone https://github.com/Gogh-Co/Gogh.git gogh
@@ -73,80 +106,107 @@ function set_colors() {
     cd installs
     ./powershell.sh
     cd $INSTALL_DIR
+    end_line
 }
 
 # removes programs I never use
 function remove_junk() {
-    echo "removing Gnome Junk"
+    echo "@@ removing junk @@"
+    repeat 80 '='; echo
     dnf remove hexchat -y
     dnf remove pidgin -y
     dnf remove thunderbird -y
     dnf remove transmission -y
     dnf remove xfburn -y
-    dnf xawtv -y
+    dnf remove xawtv -y
     dnf autoremove -y
+    end_line
 }
 
 # enables rpm fusions
 function enable_rpmfusions() {
-    echo "enabling rpm fusions"
+    echo "@@ enabling rpm fusions @@"
+    repeat 80 '='; echo
     sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
     sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
     update_software
+    end_line
 }
 
 # enables flathub
 function enable_flathub() {
-    echo "enabling flathub"
+    echo "@@ enabling flathub @@"
+    repeat 80 '='; echo
+    sudo dnf install flatpak -y
     sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    update_software
+    end_line
 }
 
 # vscode install
 function install_vscode() {
-    echo "installing vscode"
+    echo "@@ installing vscode @@"
+    repeat 80 '='; echo
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     sudo dnf check-update
-    sudo dnf install code
+    sudo dnf install code -y
+    end_line
 }
 
 # Prism Launcher (Minecraft) install
 function install_prism_launcher() {
-    echo "installing Prism Launcher (Minecraft)"
-    sudo dnf copr enable g3tchoo/prismlauncher
-    sudo dnf install prismlauncher
+    echo "@@ installing Prism Launcher (Minecraft) @@"
+    repeat 80 '='; echo
+    sudo dnf copr enable g3tchoo/prismlauncher -y
+    sudo dnf install prismlauncher -y
+    end_line
 }
 
 # Microsoft True Type fonts isntall
 function install_true_type() {
-    echo"installing True Type fonsts"
+    echo "@@ installing True Type fonsts @@"
+    repeat 80 '='; echo
+    sudo dnf install xorg-x11-font-utils -y
     sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+    end_line
 }
 
 # install codecs
 function install_codecs() {
-    echo"installing codecs"
-    update_software
-    sudo dnf install gstreamer1-libav gstreamer1-plugins-bad-free
-    sudo dnf install gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras
-    sudo dnf install gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-nonfree
-    sudo dnf install gstreamer1-plugins-good gstreamer1-plugins-ugly lame-libs lame-libs
-    sudo dnf group upgrade --with-optional Multimedia
+    echo "@@ installing codecs @@"
+    repeat 80 '='; echo
+    sudo dnf install gstreamer1-libav gstreamer1-plugins-bad-free -y
+    sudo dnf install gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras -y
+    sudo dnf install gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-nonfree -y
+    sudo dnf install gstreamer1-plugins-good gstreamer1-plugins-ugly lame-libs lame-libs -y
+    sudo dnf group upgrade --with-optional Multimedia --allowerasing -y
+    end_line
+}
+
+#install chrome
+function install_chrome() {
+    echo "@@ installing google chrome @@"
+    repeat 80 '='; echo
+    sudo dnf install fedora-workstation-repositories -y
+    sudo dnf config-manager --set-enabled google-chrome
+    sudo dnf install google-chrome-stable -y
+    end_line
 }
 
 # Nvidia drivers
 function install_nvidia() {
-    update_software
-    echo "installing nvidia driver"
+    echo "@@ installing nvidia driver @@"
+    repeat 80 '='; echo
     sudo dnf install akmod-nvidia -y
     sudo dnf install xorg-x11-drv-nvidia-cuda -y
+    end_line
 }
 
 # software install
 function install_software() {
-    echo "installing software"
-    update_software
+    echo "@@ installing software @@"
+    repeat 80 '='; echo
+    sudo dnf upgrade -y
     sudo flatpak install webcord -y
     sudo dnf install google-chrome-stable -y
     sudo dnf install file-roller -y
@@ -172,36 +232,26 @@ function install_software() {
     sudo dnf install steam -y
     sudo dnf install lutris -y
     sudo dnf install wine -y
-}
-
-# Gnome settings
-function gnome_settings() {
-    gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,close"
-    gsettings set org.gnome.mutter center-new-windows true
-}
-
-# Gnome settings for laptops
-function laptop_settings() {
-    gsettings set org.gnome.desktop.interface show-battery-percentage true
+    sudo dnf install gnome-boxes -y
+    end_line
 }
 
 function main() {
     startup
     set_configs
     update_software
-    #remove_junk
+    remove_junk
     add_wallpapers
-    set_colors
     enable_rpmfusions
     enable_flathub
     install_vscode
-    install_prism_luancher
+    install_prism_launcher
     install_true_type
     install_codecs
+    #set_colors
+    install_chrome
     install_software
-    #gnome_settings
-    #laptop_settings
-    #install_nvidia
+    install_nvidia
     echo "Please Restart the System"
 }
 
