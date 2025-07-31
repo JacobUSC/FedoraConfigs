31f2d15d797e09985dd1093720d40b5678a1a588
Jacob <jrr18@email.sc.edu>
2025-07-30 21:23:30 -0400

    removed wallpaper folder and updated readme

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -145,15 +145,6 @@ function install_chrome() {
     end_line
 }
 
-#install github desktop
-function install_github() {
-    echo "@@ Installing Github Desktop @@"
-    repeat
-    sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
-    sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
-    sudo dnf install github-desktop -y
-    end_line
-}
 
 # installs Nvidia drivers
 function install_nvidia() {
